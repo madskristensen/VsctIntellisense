e1877150434ad0652fa5456a40cd1d43a2cddf9d
Mads Kristensen <post@madskristensen.net>
2018-12-12 14:59:03 -0800

    Updates

--- src/Completion/VsctParser.cs
+++ src/Completion/VsctParser.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Xml;
 using System.Xml.XPath;
