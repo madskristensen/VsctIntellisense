cbb5406cd1693d5d10d325325b199bedde3c2d54
Mads Kristensen <madskristensen@users.noreply.github.com>
2020-10-30 12:25:30 -0700

    Added missing UI Contexts

--- src/Completion/Providers/GuidSymbolProvider.cs
+++ src/Completion/Providers/GuidSymbolProvider.cs
@@ -33,19 +33,36 @@ namespace VsctCompletion.Completion.Providers
             else if (navigator.LocalName.Equals("VisibilityItem", StringComparison.OrdinalIgnoreCase))
             {
                 yield return CreateCompletionItem("GUID_TextEditorFactory");
+                yield return CreateCompletionItem("UICONTEXT_BulkFileOperation");
+                yield return CreateCompletionItem("UICONTEXT_CloudDebugging");
                 yield return CreateCompletionItem("UICONTEXT_CodeWindow");
+                yield return CreateCompletionItem("UICONTEXT_DataSourceWindowAutoVisible");
+                yield return CreateCompletionItem("UICONTEXT_DataSourceWindowSupported");
+                yield return CreateCompletionItem("UICONTEXT_DataSourceWizardSuppressed");
                 yield return CreateCompletionItem("UICONTEXT_Debugging");
                 yield return CreateCompletionItem("UICONTEXT_DesignMode");
                 yield return CreateCompletionItem("UICONTEXT_Dragging");
                 yield return CreateCompletionItem("UICONTEXT_EmptySolution");
+                yield return CreateCompletionItem("UICONTEXT_FirstLaunchSetup");
                 yield return CreateCompletionItem("UICONTEXT_FullScreenMode");
+                yield return CreateCompletionItem("UICONTEXT_FullSolutionLoading");
+                yield return CreateCompletionItem("UICONTEXT_HistoricalDebugging");
                 yield return CreateCompletionItem("UICONTEXT_NoSolution");
                 yield return CreateCompletionItem("UICONTEXT_NotBuildingAndNotDebugging");
+                yield return CreateCompletionItem("UICONTEXT_OsWindows8OrHigher");
+                yield return CreateCompletionItem("UICONTEXT_ProjectCreating");
+                yield return CreateCompletionItem("UICONTEXT_ProjectRetargeting");
+                yield return CreateCompletionItem("UICONTEXT_RepositoryOpen");
                 yield return CreateCompletionItem("UICONTEXT_SolutionBuilding");
+                yield return CreateCompletionItem("UICONTEXT_SolutionClosing");
                 yield return CreateCompletionItem("UICONTEXT_SolutionExists");
+                yield return CreateCompletionItem("UICONTEXT_SolutionExistsAndFullyLoaded");
                 yield return CreateCompletionItem("UICONTEXT_SolutionExistsAndNotBuildingAndNotDebugging");
                 yield return CreateCompletionItem("UICONTEXT_SolutionHasMultipleProjects");
                 yield return CreateCompletionItem("UICONTEXT_SolutionHasSingleProject");
+                yield return CreateCompletionItem("UICONTEXT_SolutionOpening");
+                yield return CreateCompletionItem("UICONTEXT_SolutionOrProjectUpgrading");
+                yield return CreateCompletionItem("UICONTEXT_SynchronousSolutionOperation");
                 yield return CreateCompletionItem("UICONTEXT_ToolboxInitialized");
             }
         }
